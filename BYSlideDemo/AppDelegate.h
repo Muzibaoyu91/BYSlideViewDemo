1a3458dffd13abcf150e286476f36eaf76cd4f94
libaoyu <muzibaoyu91@gmail.com>
2016-06-28 17:04:29 +0800

    Initial Commit

---
+++ BYSlideDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BYSlideDemo
+//
+//  Created by Baoyu on 16/6/28.
+//  Copyright © 2016年 Muzibaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BYSlideDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BYSlideDemo
+//
+//  Created by Baoyu on 16/6/28.
+//  Copyright © 2016年 Muzibaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
